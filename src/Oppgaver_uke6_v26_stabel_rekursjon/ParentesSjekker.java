e97e93168df6f47c435b1548ffeab1bf012f7d98
yelyzaveta-12 <lagnyuk.liza@gmail.com>
2026-02-11 16:31:54 +0100

    parantes sjekker - i prepped the correct template given in the assignment, just have code left and junit test

--- src/Oppgaver_uke6_v26_stabel_rekursjon/ParentesSjekker.java
+++ src/Oppgaver_uke6_v26_stabel_rekursjon/ParentesSjekker.java
@@ -1,6 +1,25 @@
 package Oppgaver_uke6_v26_stabel_rekursjon;
 
+import java.util.Stack;
+
 public class ParentesSjekker {
 
+    //"{ [ ( ) ] }" er korrekt
+    //• "{ [ ( ) }" er ikke korrekt. Mangler sluttparentes ]
+    //• "[ ( ) ] }" er ikke korrekt. Mangler startparentes {
+    //• "{ [ ( ] ) }" er ikke korrekt. Sluttparentes ] kommer for tidlig.
 
+    private boolean erStartParentes(char c) {
+        return c == '(' || c == '{' || c == '[';
+    }
+
+    private boolean erSluttParentes(char c) {
+        return c == ')' || c == '}' || c == ']';
+    }
+
+    private boolean erParentesPar(char start, char slutt) {
+        return (start == '(' && slutt == ')') ||
+                (start == '{' && slutt == '}') ||
+                (start == '[' && slutt == ']');
+    }
 }
